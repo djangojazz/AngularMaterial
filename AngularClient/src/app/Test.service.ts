fdf5ff24111095c4fe0cde39f8cbfcb2172b2685
Brett Morin <brett.morin@bluevolt.com>
2021-05-23 11:00:43 -0700

    moved code to have a place for C#

--- AngularClient/src/app/Test.service.ts
+++ AngularClient/src/app/Test.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ɵɵtrustConstantResourceUrl } from '@angular/core';
 import { Observable, of, timer } from 'rxjs';
 import { Entry } from './Models/entry.model';
 
@@ -10,8 +10,7 @@ export class TestService {
 constructor() { }
 
   public getItem(): Observable<Entry> {
-    timer(2000);
-    return of({ id: 1, desc: 'test'});
+    return timer(2000).pipe(map => of({ id: 1, desc: 'test'}));
   }
 
 }
